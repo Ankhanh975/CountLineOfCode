a5bfe4ff9fb1b30b6626a6093bdd54329fd64295
Gold_Apple_Vn <ankhanh1566@gmail.com>
2021-08-26 21:45:56 +0700

    Up

---
+++ main.py
@@ -0,0 +1,21 @@
+import os
+import sys
+import time
+
+
+def run(self, filepath):
+    line = 0
+    AllFiles = os.listdir()
+    for file in AllFiles:
+        pass
+        if os.path.isfile(f):
+            pass
+    print(x, type(x), type(x[0]))
+    listOfFiles = [f for f in os.listdir() ]
+    print(listOfFiles)
+
+    return line
+
+
+if __name__ == '__main__':
+    run(None, "C:\\Users\\Admin\\Desktop\\HTML\\CuontLineOfCode")
